d920dc1a23b1d24f927b1b4942c63e97d837a402
Vishal Singh <136308443+Vishalsingh061@users.noreply.github.com>
2024-03-30 01:43:31 +0530

    Update Signup.jsx

--- client/src/screens/Signup.jsx
+++ client/src/screens/Signup.jsx
@@ -20,7 +20,7 @@ export default function Signup() {
             return [latitude, longitude]
         })
         let [lat, long] = latlong
-        const response = await fetch("http://localhost:5000/api/auth/getlocation", {
+        const response = await fetch("https://go-food-mern-app.vercel.app/api/auth/getlocation", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -35,7 +35,7 @@ export default function Signup() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+        const response = await fetch("https://go-food-mern-app.vercel.app/api/auth/createuser", {
             // credentials: 'include',
             // Origin:"http://localhost:3000/login",
             method: 'POST',
