2dc0c78947bcd79b5a4986d31579e14a0b8625c3
Vishal Singh <136308443+Vishalsingh061@users.noreply.github.com>
2024-03-30 02:22:11 +0530

    Update Home.jsx

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -73,7 +73,7 @@ export default function Home() {
                     {data.CategoryName}
                   </div>
                   <hr id="hr-success" style={{ height: "4px", backgroundImage: "-webkit-linear-gradient(left,rgb(0, 255, 137),rgb(0, 0, 0))" }} />
-                  {foodItems.length !== 0 ? foodItems.filter(
+                  {foodItems !== [] ? foodItems.filter(
                     (items) => (items.CategoryName === data.CategoryName) && (items.name.toLowerCase().includes(search.toLowerCase())))
                     .map((filterItems) => {
                       return (
