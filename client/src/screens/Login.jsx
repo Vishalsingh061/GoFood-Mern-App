b92e5a347b00cf51ff0400ca5c21f706ef31535d
Vishal Singh <136308443+Vishalsingh061@users.noreply.github.com>
2024-03-30 01:42:26 +0530

    Update Login.jsx

--- client/src/screens/Login.jsx
+++ client/src/screens/Login.jsx
@@ -9,7 +9,7 @@ export default function Login() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:5000/api/auth/login", {
+            const response = await fetch("https://go-food-mern-app.vercel.app/api/auth/login", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
