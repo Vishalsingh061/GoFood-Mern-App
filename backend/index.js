cd2ab11e978ca514f209cb61fbcaf01b02a6bb86
Vishal Singh <136308443+Vishalsingh061@users.noreply.github.com>
2024-03-30 02:04:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,6 @@ mongoDB((err, data, CatData) => {
 });
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
